fb9749ed8bafe15a036650f746263f779da057cb
kanatapple <edamame1224@gmail.com>
2017-04-01 23:48:26 +0900

    マスに数字が表示されるようにした

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Square extends Component {
   render() {
     return (
-      <button className="square"></button>
+      <button className="square">{this.props.value}</button>
     );
   }
 }
